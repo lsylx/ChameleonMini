4110db2ba09f4b309bf49ac62fe2896fb4773a69
lsylx <1252768804@qq.com>
2020-04-23 12:34:01 +0800

    new branch

--- Firmware/Chameleon-Mini/LUFADescriptors.c
+++ Firmware/Chameleon-Mini/LUFADescriptors.c
@@ -211,7 +211,7 @@ const USB_Descriptor_String_t PROGMEM ManufacturerString = {
 const USB_Descriptor_String_t PROGMEM ProductString = {
     .Header                 = {.Size = USB_STRING_LEN(17), .Type = DTYPE_String},
 
-    .UnicodeString          = L"ChameleonG-Zhener"
+    .UnicodeString          = L"ChameleonG-lsy"
 };
 
 /** This function is called by the library when in device mode, and must be overridden (see library "USB Descriptors"
